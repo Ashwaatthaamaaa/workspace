a7a191a0d2c2d6abf2c995b301e616f002105aa0
Sarthak Kumar <sarthakk145@gmail.com>
2025-04-29 07:32:13 +0000

    DONE

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import { defineConfig } from "vite";
   // https://vitejs.dev/config/
   export default defineConfig({
       plugins: [react()],
+      server: {
+        allowedHosts: ['rhqf9w-5173.csb.app'], 
+      },
   });
   
